d165618e2c21ed96eccaaf31c7678f04b50e1124
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-04-18 01:05:21 +0300

    models update

--- server/tasks.py
+++ server/tasks.py
@@ -1,6 +1,8 @@
 from databaseconfig import db
 
 class Task(db.Model):
+    __tablename__ = 'task'
+    
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text)
